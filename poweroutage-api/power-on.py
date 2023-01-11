32c62e92c06d8fe3f0b39b321219e1dc902c65ff
j t f <rrmutt@gmail.com>
2023-01-11 10:47:27 -0800

    add power-on and power-off test scripts (UNTESTED)

---
+++ poweroutage-api/power-on.py
@@ -0,0 +1,32 @@
+import os
+import time
+import RPi.GPIO as GPIO
+USE_HW = True
+USE_VIM = True
+USE_RPI = False
+
+
+#### Everything happens here
+if __name__ == '__main__':
+
+    if USE_HW:
+
+        if USE_RPI:
+            RELAY_PIN = 18 # BCM 18, GPIO.1 physical pin 12
+        elif USE_VIM:
+            RELAY_PIN = 37
+
+        # Relay is connected NORMALLY CLOSED so gpio.cleanup() leaves it SET.
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
+        GPIO.setup(RELAY_PIN,GPIO.OUT)
+        # power on on startup
+        GPIO.output(RELAY_PIN, GPIO.HIGH)
+        print("setting GPIO {} to GPIO.HIGH".format(RELAY_PIN))
+        
+        time.sleep(1)
+        try:
+            GPIO.cleanup()  
+        except RuntimeWarning:
+            print('Caught warning')
+
